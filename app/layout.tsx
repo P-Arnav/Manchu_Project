beec77f242c3222a2565cca8e3bc263fe3205f83
P-Arnav <panwararnav21@gmail.com>
2025-11-13 08:47:08 +0530

    changed for mobile view

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className="bg-black text-white">
         {/* Sticky Navbar */}
-        <header className="w-full bg-black/90 border-b border-gray-800 px-4 py-4">
+        <header className="w-full bg-black/90 border-b border-gray-800 px-4 py-2">
           <div className="max-w-6xl mx-auto flex flex-col sm:flex-row sm:items-center sm:justify-between gap-3">
 
             {/* Left side (title) */}
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             {/* Right side (links) */}
             <nav className="flex gap-6 text-gray-300 justify-center sm:justify-end text-lg">
               <Link href="/" className="hover:text-blue-400 transition">Home</Link>
-              <Link href="/gallery" className="hover:text-blue-400 transition">Gallery</Link>
+              <Link href="/documents/id" className="hover:text-blue-400 transition">Gallery</Link>
               <Link href="/search" className="hover:text-blue-400 transition">Search</Link>
               <Link href="/translate" className="hover:text-blue-400 transition">Translate</Link>
             </nav>
